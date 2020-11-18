72fcf823f809718d93bcd86f0465c2fb6effc544
kontel <kontel@mail.ru>
2020-11-18 16:47:17 +0300

    раз в секунду отправка данных по всем модульным выходам

--- Src/do_module.c
+++ Src/do_module.c
@@ -10,6 +10,11 @@
 extern uint16_t do_mod_cnt;
 extern struct do_mod* do_modules_ptr;
 
+static uint16_t upd_next_do_lim = 0xFFFF;
+static uint16_t upd_do_mod_num = 0;
+static uint16_t upd_do_tmr = 0;
+
+
 void do_mod_init_values(struct do_mod *mod) {
 	for(uint8_t i=0;i<MOD_DO_OUT_CNT;++i) {
 		mod->do_state[i] = 0;
@@ -19,6 +24,8 @@ void do_mod_init_values(struct do_mod *mod) {
 	mod->update_data = 0;
 	mod->link_state = 0;
 	mod->heartbeat_cnt = 0;
+
+	if(do_mod_cnt) upd_next_do_lim = 1000/do_mod_cnt;
 }
 
 void do_mod_cycle() {
@@ -28,6 +35,18 @@ void do_mod_cycle() {
 				do_modules_ptr[i].prev_do_state[j] = do_modules_ptr[i].do_state[j];
 				do_modules_ptr[i].update_data = 1;
 			}
+		}
+	}
+
+	// в течение одной секунды с равными промежутками постоянно передаются данные
+	// всем модулям чтобы они контролировал связь с контроллером (на случай когда данные не меняются)
+	if(do_mod_cnt) {
+		upd_do_tmr++;
+		if(upd_do_tmr>=upd_next_do_lim) {
+			upd_do_tmr = 0;
+			do_modules_ptr[upd_do_mod_num].update_data = 1;
+			upd_do_mod_num++;
+			if(upd_do_mod_num>=do_mod_cnt) upd_do_mod_num=0;
 		}
 	}
 }
